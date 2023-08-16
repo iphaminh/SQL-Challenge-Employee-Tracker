042888cf1ca552784da499c6ea25d9db8623427f
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-16 16:02:58 -0400

    adding comments for functionality

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Importing required modules
 const inquirer = require('inquirer');
 const connection = require('./config/connection');
 
+// Function to retrieve all departments from the database
 function getDepartments() {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM department', (err, results) => {
@@ -10,6 +12,7 @@ function getDepartments() {
     });
 }
 
+// Function to retrieve all roles from the database
 function getRoles() {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM role', (err, results) => {
@@ -19,6 +22,7 @@ function getRoles() {
     });
 }
 
+// Function to retrieve all employees from the database
 function getEmployees() {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM employee', (err, results) => {
@@ -28,21 +32,25 @@ function getEmployees() {
     });
 }
 
+// Function to add a new role to the database
 async function addRole() {
     const departments = await getDepartments();
     const departmentChoices = departments.map(dept => ({ name: dept.name, value: dept.id }));
 
     inquirer.prompt([
+        // Prompt for role title
         {
             type: 'input',
             name: 'roleTitle',
             message: 'Enter the title of the new role:'
         },
+        // Prompt for role salary
         {
             type: 'input',
             name: 'roleSalary',
             message: 'Enter the salary for this role:'
         },
+        // Prompt to select department for the role
         {
             type: 'list',
             name: 'departmentId',
@@ -61,6 +69,7 @@ async function addRole() {
     });
 }
 
+// Function to add a new employee to the database
 async function addEmployee() {
     const roles = await getRoles();
     const roleChoices = roles.map(role => ({ name: role.title, value: role.id }));
@@ -70,22 +79,26 @@ async function addEmployee() {
     employeeChoices.unshift({ name: 'No Manager', value: null });
 
     inquirer.prompt([
+        // Prompt for employee's first name
         {
             type: 'input',
             name: 'firstName',
             message: 'Enter the first name of the employee:'
         },
+        // Prompt for employee's last name
         {
             type: 'input',
             name: 'lastName',
             message: 'Enter the last name of the employee:'
         },
+        // Prompt to select role for the employee
         {
             type: 'list',
             name: 'roleId',
             message: 'Select the role for this employee:',
             choices: roleChoices
         },
+        // Prompt to select manager for the employee
         {
             type: 'list',
             name: 'managerId',
@@ -104,6 +117,7 @@ async function addEmployee() {
     });
 }
 
+// Main function to prompt user for actions
 function init() {
     inquirer.prompt([
         {
@@ -152,6 +166,7 @@ function init() {
     });
 }
 
+// Function to display all roles in the database
 function viewAllRoles() {
     connection.query('SELECT * FROM role', (err, results) => {
         if (err) throw err;
@@ -160,6 +175,7 @@ function viewAllRoles() {
     });
 }
 
+// Function to display all departments in the database
 function viewAllDepartments() {
     connection.query('SELECT * FROM department', (err, results) => {
         if (err) throw err;
@@ -168,6 +184,7 @@ function viewAllDepartments() {
     });
 }
 
+// Function to display all employees in the database
 function viewAllEmployees() {
     connection.query('SELECT * FROM employee', (err, results) => {
         if (err) throw err;
@@ -176,7 +193,7 @@ function viewAllEmployees() {
     });
 }
 
-
+// Function to add a new department to the database
 function addDepartment() {
     inquirer.prompt([
         {
@@ -194,6 +211,7 @@ function addDepartment() {
     });
 }
 
+// Function to update the role of an existing employee
 async function updateEmployeeRole() {
     const employees = await getEmployees();
     const employeeChoices = employees.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }));
