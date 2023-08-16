9baa9620d917d17a642e7629b766e6de73b3292c
iphaminh <126002737+iphaminh@users.noreply.github.com>
2023-08-16 15:13:45 -0400

    change localhost

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'employee_tracker'
